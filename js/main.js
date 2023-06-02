19a949c6399da338ec782754a9892bfe73b60d91
bfhr2003 <b_hortuarodrig152861@fanshaweonline.ca>
2023-06-02 18:17:23 -0400

    Improving my JS file

--- js/main.js
+++ js/main.js
@@ -1,14 +1,1 @@
 console.log("JS file connected");
-
-//variables
-
-const flavours = document.querySelectorAll("#flavours object");
-console.log(flavours);
-
-function logId() {
-    console.log(this.id);
-}
-
-
-//Event Listeners
-flavours.forEach(flavour => flavour.addEventListener("click", logId));
